cea673a089b28482aeb355dd08f66cb75395e50e
Fahmi <fahminurfalah26@gmail.com>
2022-01-13 22:55:56 +0700

    add validator joi library

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/inMemory/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -12,7 +15,13 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
